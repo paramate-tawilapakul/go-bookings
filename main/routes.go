a126200da4af6341ed93359158c9f7f0879fd97a
moohoozaa <paramate.php@gmai.com>
2022-02-25 17:35:14 +0700

    move pkg to internal folder

--- main/routes.go
+++ main/routes.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bookings/pkg/config"
-	"bookings/pkg/handlers"
+	"bookings/internal/config"
+	"bookings/internal/handlers"
 	"net/http"
 
 	"github.com/go-chi/chi/middleware"
@@ -22,6 +22,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/majors-suite", http.HandlerFunc(handlers.Repo.Majors))
 	mux.Get("/search-availability", http.HandlerFunc(handlers.Repo.Availability))
 	mux.Post("/search-availability", http.HandlerFunc(handlers.Repo.PostAvailability))
+	mux.Post("/api/search-availability-json", http.HandlerFunc(handlers.Repo.AvailabilityJSON))
 	mux.Get("/contact", http.HandlerFunc(handlers.Repo.Contact))
 
 	mux.Get("/make-reservation", http.HandlerFunc(handlers.Repo.Reservation))
